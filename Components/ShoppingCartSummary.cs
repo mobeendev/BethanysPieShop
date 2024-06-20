f3eb3164740d8bb2c70364c850b9a4810ce35d9d
abdulmobeen <mobeendev@gmail.com>
2024-06-20 17:01:36 +0300

    mend

--- Components/ShoppingCartSummary.cs
+++ Components/ShoppingCartSummary.cs
@@ -15,13 +15,9 @@ namespace BethanysPieShop.Components
 
         public IViewComponentResult Invoke()
         {
-            // var items = new List<ShoppingCartItem>() { new ShoppingCartItem(), new ShoppingCartItem() };
-
             var items = _shoppingCart.GetShoppingCartItems();
             _shoppingCart.ShoppingCartItems = items;
-
             var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
-            Console.WriteLine("Invoke::  shoppingCartViewModel!", _shoppingCart.GetShoppingCartTotal());
 
             return View(shoppingCartViewModel);
         }
